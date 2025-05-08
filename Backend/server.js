63f7b2651320f76204878f22021c2b977fad251e
joe <jathinsomanna@gmail.com>
2025-05-08 17:39:23 +0530

    ok

---
+++ Backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/products', productRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Skincare E-Commerce API is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
